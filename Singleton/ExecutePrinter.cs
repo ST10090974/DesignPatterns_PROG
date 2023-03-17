28626bc15afab170fac47f9b21c2b7fbbce4a484
lab_services_student <ST10090974@vcconnect.edu.za>
2023-03-17 06:25:18 +0000

    Add project files.

---
+++ Singleton/ExecutePrinter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Singleton
+{
+    internal class ExecutePrinter
+    {
+        static void Main(string[] args)
+        {
+            Printer prin1 = Printer.GetInstance();
+            Printer prin2 = Printer.GetInstance();
+
+            Console.WriteLine(prin1.GetHashCode());
+            Console.WriteLine(prin2.GetHashCode());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singleton/Printer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Singleton
+{
+    internal class Printer
+    {
+
+        private  Printer() { }
+
+		private static Printer objPrinter;
+
+		public static Printer GetInstance()
+		{
+			if (objPrinter == null)
+			{
+				objPrinter= new Printer();
+			}
+			return objPrinter; 
+		}
+
+
+	}
+}
